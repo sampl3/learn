e8810b19d97aef5a753bf219c7fdd300c3dbcccb
phildit <phildit@gmail.com>
2016-02-06 22:59:24 +0100

    git tests

--- gulp/serve.js
+++ gulp/serve.js
@@ -2,3 +2,20 @@ var  gulp = require('gulp');
 var serve = require('gulp-serve');
 
 gulp.task('serve',  serve('app'));
+
+var shell=require('gulp-shell');
+var util=require('gulp-util');
+var args = require('yargs').argv;
+gulp.task('a', function () {
+  return gulp.src('app/**/*.' + util.env.suffix, {read: false})
+    .pipe(shell([
+      'echo  <%= f(file.path) %>',
+      'ls -l <%= file.path %>'
+    ], {
+      templateData: {
+        f: function (s) {
+          return s.replace(/$/, '.bak')
+        }
+      }
+    }))
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,6 @@
+var requireDir = require('require-dir');
+var dir = requireDir('./gulp');
+
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
